068249143626979dfd94daaa17ef60451dba845e
Lucascanna <lucascannapieco.94@gmail.com>
2019-07-06 16:42:31 +0200

    refactor

--- index.js
+++ index.js
@@ -5,13 +5,41 @@ const zip = require('lodash.zip')
 const digitsMap = require('./lib/digits')
 const readFileAsArrayOfStrings = require('./lib/fileReader')
 
-function separateOcrDigits(ocrLines) {
+module.exports = ocrParser
+
+function ocrParser(filename) {
+    const fileLines = readFileAsArrayOfStrings(filename)
+    const accountNumbersRepresentations = groupLinesBelongingToOneAccountNumber(fileLines)
+    return accountNumbersRepresentations
+        .map(accounteNumberRepresentation => parseAccountNumber(accounteNumberRepresentation))
+        .join('\n')
+}
+
+function groupLinesBelongingToOneAccountNumber(fileLines) {
+    return fileLines.reduce((accumulator, currentLine, currentIndex, initialArray) => {
+        if (currentIndex % 4 == 0) {
+            accumulator.push(initialArray.slice(currentIndex,currentIndex+4))
+        }
+        return accumulator
+    }, [])
+}
+
+function parseAccountNumber(accountNumberRepresentation) {
+    const digitsRepresentations = separateDigits(accountNumberRepresentation)
+    const parsedAccountNumber = digitsRepresentations
+        .map(digitRepresentation => parseDigit(digitRepresentation))
+        .join('')
+    const validatedAccountNumber = validateAccountNumber(parsedAccountNumber)
+    return validatedAccountNumber
+}
+
+function separateDigits(ocrLines) {
     const digitRows = ocrLines.map(line => line.match(/.{3}/g))
     const digits = zip(...digitRows)
     return digits
 }
 
-function fromOcrToNumber(digit) {
+function parseDigit(digit) {
     const flatDigit = digit.join()
     const number = digitsMap[flatDigit]
     if (!number) {
@@ -20,40 +48,29 @@ function fromOcrToNumber(digit) {
     return number
 }
 
-function parseOCR(ocrLines) {
-    const ocrDigits = separateOcrDigits(ocrLines)
-    return ocrDigits
-        .map(digit => fromOcrToNumber(digit))
-        .join('')
-}
-
-function validateOCR(accountNumberAsString) {
-    if (accountNumberAsString.includes('?')) {
+function validateAccountNumber(accountNumberAsString) {
+    if (isIll(accountNumberAsString)) {
         return `${accountNumberAsString} ILL`
     }
-    const checksum = accountNumberAsString
-        .split('')
-        .reverse()
-        .map((currentNumber, currentIndex) => parseInt(currentNumber)*(currentIndex+1))
-        .reduce((total, current) => total += current, 0)
-    if (checksum % 11 !== 0) {
+    if (isErroneous(accountNumberAsString)) {
         return `${accountNumberAsString} ERR`
     }
     return accountNumberAsString
 }
 
-function main(filename) {
-    const fileLines = readFileAsArrayOfStrings(filename)
-    const groupedOcrLines = fileLines.reduce((accumulator, currentLine, currentIndex, initialArray) => {
-        if (currentIndex % 4 == 0) {
-            accumulator.push(initialArray.slice(currentIndex,currentIndex+4))
-        }
-        return accumulator
-    }, [])
-    return groupedOcrLines
-        .map(parseOCR)
-        .map(validateOCR)
-        .join('\n')
+function isIll(accountNumberAsString) {
+    return accountNumberAsString.includes('?')
 }
 
-module.exports = main
+function isErroneous(accountNumberAsString) {
+    const checksum = computeChecksum(accountNumberAsString)
+    return checksum % 11 !== 0
+}
+
+function computeChecksum(accountNumberAsString) {
+    return accountNumberAsString
+        .split('')
+        .reverse()
+        .map((currentNumber, currentIndex) => parseInt(currentNumber)*(currentIndex+1))
+        .reduce((total, current) => total += current, 0)
+}
